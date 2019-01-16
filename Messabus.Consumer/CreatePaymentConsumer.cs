f4f49f5f4b3d99a8e15ff9b310f39f7b9533c1cf
Henrique Andre Felipe da Costa <henriafc@outlook.com>
2019-01-16 20:04:21 -0200

    ajuste no consumer

---
+++ Messabus.Consumer/CreatePaymentConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Messaging;
+using System;
+using System.Threading.Tasks;
+
+namespace Messabus.Consumer 
+{
+    public class CreatePaymentConsumer : IConsumer<ICreatePayment>
+    {
+        public Task Consume(ConsumeContext<ICreatePayment> context)
+        {
+            ICreatePayment payment = context.Message;
+
+            Console.WriteLine($"Payment Order: " +
+                $"Account Credit {payment.CreditAccount}, " +
+                $"Account Debit {payment.DebitAccount}, " +
+                $"Value {payment.Value}, " +
+                $"Payment Type {payment.PaymentType}");
+
+            context.Publish<IPaymentCreated>(new
+            {
+                CreditAccount = payment.CreditAccount,
+                DebitAccount = payment.DebitAccount,
+                Value = payment.Value,
+                PaymentType = payment.PaymentType
+            });
+
+            return Task.FromResult(context);
+        }
+    }
+}
---
+++ Messabus.Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using GreenPipes;
+using System;
+
+namespace Messabus.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Consumer Payment";
+            Console.WriteLine("Consumer Payment");
+
+            RunMessageBus();
+        }
+
+        private static void RunMessageBus()
+        {
+            IBusControl rabbitBusControl = Bus.Factory.CreateUsingRabbitMq(rabbit =>
+            {
+                IRabbitMqHost rabbitMqHost = rabbit.Host(new Uri("rabbitmq://localhost:5672/payment"), settings =>
+                {
+                    settings.Password("guest");
+                    settings.Username("guest");
+                });
+
+                rabbit.ReceiveEndpoint(rabbitMqHost, "superdigital.payments.orderpayments", conf =>
+                {
+                    conf.UseRetry(retryConfig => {
+                        retryConfig.Immediate(5);
+                        retryConfig.Handle<Exception>();
+                    });
+                       
+                    conf.Consumer<CreatePaymentConsumer>(consumerConfig => 
+                    {
+                        
+                        consumerConfig.UseRetry(retryConfig => {
+                            retryConfig.Immediate(5);
+                            retryConfig.Ignore<ArgumentNullException>();
+                        });
+                     
+                    });
+                });
+            });
+
+            rabbitBusControl.Start();
+            Console.ReadKey();
+
+            rabbitBusControl.Stop();
+        }
+    }
+}
